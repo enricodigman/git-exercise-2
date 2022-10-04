b01d1c5fdb2d1dcfc9a397d66e5f781d26bc7f3b
Enrico Digman <essdigman@gmail.com>
2022-10-05 07:32:15 +0800

    Changed palindrome function

--- lib.js
+++ lib.js
@@ -8,10 +8,10 @@ const isPalindrome = (str) => {
     let str2 = str.length - 1;
 
     for (let i = 0; i < str.length / 2; i++) {
-        if (str[i].toUpperCase() === str[str2 - i].toUpperCase()) {
+        if (str.charAt(i).toUpperCase() === str.charAt(str2 - i).toUpperCase()) {
             return true;
         }
     }
-    
+
     return false;
 };
