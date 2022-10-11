4fddc4a8fd396c4e2129a3045815cb45a1a81c77
Enrico Digman <essdigman@gmail.com>
2022-10-11 09:13:08 +0800

    Implemented prettier module

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib');
 console.log(square(5));
 console.log(fahrenheit(40));
 console.log(rectangleArea(50, 6));
-console.log(isPalindrome("RoTator")); //true
-console.log(isPalindrome("airplane")); //false
+console.log(isPalindrome('RoTator')); //true
+console.log(isPalindrome('airplane')); //false
--- lib.js
+++ lib.js
@@ -1,24 +1,24 @@
 const square = (n) => Math.pow(n, 2);
 
-const fahrenheit = (celsius) => (celsius * 1.8) + 32;
+const fahrenheit = (celsius) => celsius * 1.8 + 32;
 
 const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => {
-    let str2 = str.length - 1;
+  let str2 = str.length - 1;
 
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str.charAt(i).toUpperCase() === str.charAt(str2 - i).toUpperCase()) {
-            return true;
-        }
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str.charAt(i).toUpperCase() === str.charAt(str2 - i).toUpperCase()) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 };
 
 module.exports = {
-    square,
-    fahrenheit,
-    rectangleArea,
-    isPalindrome
+  square,
+  fahrenheit,
+  rectangleArea,
+  isPalindrome,
 };
