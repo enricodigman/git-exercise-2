9e66d4ece9a3f3f43d39f6ef1272ba2b200d6bea
Enrico Digman <essdigman@gmail.com>
2022-10-26 11:21:53 +0800

    Converted to esm module

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import * as examples from './lib.mjs'
+
+console.log(examples.square(5))
+console.log(examples.fahrenheit(40))
+console.log(examples.rectangleArea(50, 6))
+console.log(examples.isPalindrome('RoTator'))
+console.log(examples.isPalindrome('airplane'))
